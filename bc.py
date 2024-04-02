b8d4ae9026c6fecf60247b401aad9a7a45eedb09
GDharan10 <161794250+GDharan10@users.noreply.github.com>
2024-04-02 18:29:13 +0530

    Add files via upload

--- bc.py
+++ bc.py
@@ -5,6 +5,11 @@ import io
 import re
 import pandas as pd
 from sqlalchemy import create_engine 
+import streamlit as st
+from streamlit_option_menu import option_menu
+import os
+import cv2
+import matplotlib.pyplot as plt
 
 engine = create_engine("postgresql+psycopg2://postgres:1005@localhost/bizcardx_data")
 
@@ -19,13 +24,6 @@ image = Image.open(image_path)
 image = image.convert('RGB')
 
 
-# Convert the image to binary data
-def img_to_binary(image):
-    image_binary = io.BytesIO()
-    image.save(image_binary, format='JPEG')
-    image_binary = image_binary.getvalue()
-    return image_binary
-
 def extracted_text(details):
     data = {
         "name": [],
@@ -37,8 +35,7 @@ def extracted_text(details):
         "city": [],
         "state": [],
         "pincode": [],
-        "company": [],
-        "image" : img_to_binary(image)
+        "company": []
     }
     
     for i in range(len(details)):
@@ -87,11 +84,130 @@ def extracted_text(details):
     data["company"] = [" ".join(data["company"])]
     return data
 
-# converting to dictionary
-data = extracted_text(image_data)
-# Converting dictionary to DataFrame
-df = pd.DataFrame(data)
-# Storing DataFrame in SQL table
-df.to_sql('business_card', engine, if_exists='append', index=False)
+def store_data(data):
+    # Converting dictionary to DataFrame
+    df = pd.DataFrame(data)
+    # Storing DataFrame in SQL table
+    df.to_sql('business_card', engine, if_exists='append', index=False)
 
 
+
+# Streamlit part
+st.set_page_config(page_title= "BizCardX",
+                   page_icon= '💼',
+                   layout= "wide",)
+
+text = 'BizCardX'   
+st.markdown(f"<h2 style='color: white; text-align: center;'>{text} </h2>", unsafe_allow_html=True)
+
+st.markdown(f""" <style>.stApp {{
+                    background: url('https://img.freepik.com/free-vector/gradient-golden-luxury-business-card-template_23-2149035722.jpg?w=740&t=st=1712021022~exp=1712021622~hmac=af788945480688006d710a5bda024f6d2b87d50990c0edd91b1e0c7c4b205ddf');   
+                    background-size: cover}}
+                 </style>""",unsafe_allow_html=True)
+
+col1,col2 = st.columns([1,4])
+with col1:
+    menu = option_menu("Menu", ["Home","Upload","Database"], 
+                    icons=["house",'cloud-upload', "list-task"],
+                    menu_icon="cast",
+                    default_index=0,
+                    styles={"icon": {"color": "orange", "font-size": "20px"},
+                            "nav-link": {"font-size": "15px", "text-align": "left", "margin": "-2px", "--hover-color": "#FFFFFF"},
+                            "nav-link-selected": {"background-color": "#225154"}})
+    if menu == 'Upload':
+        upload_menu = option_menu("Upload", ['Predefined','Undefined'],                        
+                        menu_icon='cloud-upload',
+                        default_index=0,
+                        styles={"icon": {"color": "orange", "font-size": "20px"},
+                                "nav-link": {"font-size": "15px", "text-align": "left", "margin": "-2px", "--hover-color": "#FFFFFF"},
+                                "nav-link-selected": {"background-color": "#225154"}})
+    
+    if menu == 'Database':
+        Database_menu = option_menu("Database", ['Modify','Delete'], 
+                        
+                        menu_icon="list-task",
+                        default_index=0,
+                        styles={"icon": {"color": "orange", "font-size": "20px"},
+                                "nav-link": {"font-size": "15px", "text-align": "left", "margin": "0px", "--hover-color": "#FFFFFF"},
+                                "nav-link-selected": {"background-color": "#225154"}})
+
+
+
+with col2:
+    if menu == 'Home':
+        col3,col4 = st.columns([3,2])
+        with col3:
+            st.header('Welcome to business card application')
+            st.subheader(':orange[About the App:]')
+            home_text = (f'''In this Streamlit web app, you can upload an image of a business 
+                         card and extract relevant information from it using EasyOCR. You can view, 
+                         modify, or delete the extracted data in this app. Additionally, the app would 
+                         allow users to save the extracted information into a database alongside the 
+                         uploaded business card image. The database would be capable of storing multiple 
+                         entries, each with its own business card image and the extracted information.''')
+                         
+            st.markdown(f"<h4 text-align: left;'>{home_text} </h4>", unsafe_allow_html=True)
+            st.subheader(":orange[Technologies Used:]")
+            tech_text =('  EasyOCR, Python, SQL, Streamlit')
+            st.markdown(f"<h4 text-align: left;'>{tech_text} </h4>", unsafe_allow_html=True)
+        with col4:
+            home_image = Image.open('C:\\GD\\Notes\\DS Class\\DTM15\\Project\\Guvi project\\3 BizCardX Extracting Business Card Data with OCR\\Business Cards\\blue.jpg')
+            st.write('')
+            st.write('')
+            st.image(home_image)
+
+
+    if menu == 'Upload':
+        
+        path = False
+        if upload_menu == 'Predefined':
+            col3,col4 = st.columns([2,2])
+            with col3:
+                uploaded_file = st.file_uploader("**Choose a file**", type=["jpg", "png", "jpeg"])
+                extract = st.button("Extract and Upload")
+                if uploaded_file is not None:
+                    image_path = os.getcwd()+ "\\"+"Business Cards"+"\\"+ uploaded_file.name
+                    image = Image.open(image_path)
+                    col3.image(image)
+                    path = True
+            
+            with col4:
+                    st.write('')
+                    st.write('')
+                    st.info(f'''i) Kindly upload the image in JPG, PNG, or JPEG format.       
+                            ii) Click the "**Extract and Upload**" button to extract text from the image and upload the extracted text details to the database.''', icon="ℹ️")
+                    if path:                
+                        image_details = extracted_text(image_path)
+                        if extract:
+                            img = cv2.imread(image_path)
+                            reader = easyocr.Reader(['en'])
+                            result = reader.readtext(image_path)
+                            for detection in result:    
+                                top_left =tuple([int(val) for val in detection[0][0]])
+                                bottom_right =tuple([int(val) for val in detection[0][2]])
+                                text = detection[1]
+                                font =cv2.FONT_HERSHEY_SIMPLEX
+                                img = cv2.rectangle(img, top_left, bottom_right, (0,255,0), 2)
+                                img = cv2.putText(img, text, top_left, font, 1, (255,0,0),1, cv2.LINE_AA)
+                                plt.figure(figsize=(20,20))
+                                
+                            st.write("")
+                            st.write("")
+                            st.subheader("Extracted Text")
+                            st.image(img)            
+                            
+                        with col3:
+                            if extract:
+                                st.write('**Name** :',image_details['name'])
+                                st.write('**Designation** :', image_details['designation'])
+                                st.write('**Company Name** :', image_details['company'])
+                                st.write('**Contact Number** :', image_details['contact'])
+                                st.write('**E-mail** :', image_details['email'])
+                        with col4:
+                            if extract:
+                                st.write('**Website** :', image_details['website'])
+                                st.write('**Street** :', image_details['street'])
+                                st.write('**City** :', image_details['city'])
+                                st.write('**State** :', image_details['state'])
+                                st.write('**Pincode** :', image_details['pincode'])
+                                
